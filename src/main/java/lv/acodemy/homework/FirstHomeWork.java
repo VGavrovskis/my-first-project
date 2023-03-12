1d3a4c17bb64e1ce5fc9504f90e7c60777770d72
VGavrovskis <v_gavrovskis@inbox.lv>
2023-03-12 11:26:45 +0200

    [homework_016032023]: Add classroom progress

--- src/main/java/lv/acodemy/homework/FirstHomeWork.java
+++ src/main/java/lv/acodemy/homework/FirstHomeWork.java
@@ -1,19 +1,17 @@
 package lv.acodemy.homework;
 
-import java.io.PrintStream;
-
 public class FirstHomeWork {
     public static void main(String[] args) {
-        int CircleDiameter = 10;
-        int CircleRadius = 5;
+        int circleDiameter = 10;
+        int circleRadius = 5;
         double pi = 3.14;
 
-        System.out.println("Circle S = " + CircleRadius * CircleRadius * pi);
-        System.out.println("Circle S = " + CircleDiameter * CircleDiameter / 4 * pi);
+        System.out.println("Circle S = " + circleRadius * circleRadius * pi);
+        System.out.println("Circle S = " + circleDiameter * circleDiameter / 4 * pi);
 
-        byte MonthInYear = 12;
-        int DaysInYear = 365;
-        PrintStream printf = System.out.printf("in Year month = " + MonthInYear +" " + "and days = " + DaysInYear);
+        byte monthInYear = 12;
+        int daysInYear = 365;
+        System.out.printf("in Year month = " + monthInYear +" " + "and days = " + daysInYear);
 
         short num1 = 10;
         short num2 = -10;
@@ -21,7 +19,7 @@ public class FirstHomeWork {
 
         char firstNameLetter = 'V';
         char firstLSurnameLetter = 'G';
-        String initialNameSurname = firstNameLetter + "" + firstLSurnameLetter;
+        String initialNameSurname = firstNameLetter + " " + firstLSurnameLetter;
         System.out.println(initialNameSurname);
 
         String name = "Vjaceslavs";
@@ -32,6 +30,5 @@ public class FirstHomeWork {
 
         System.out.printf("My name is %s. I like %s.\nAt the moment I'm a %s for about %d years.\nNow is the time for change and I want to be a better %s!", name, hobby, job, jobYear, futureJob);
     }
-
 }
 
